ceb4824645a7b7085b9a626019b6efb63fef0deb
Makarova-Lida <lida-km@ya.ru>
2025-06-29 14:08:13 +0300

    изучение математических функций

--- proba_v2.js
+++ proba_v2.js
@@ -1,8 +1,28 @@
 const one = 'Naharis'
 const two = 'Mormont'
 const three = 'Sand'
-
+const fruitsCount = -2309
+const number = 923.2238
+const name = 'JavaScript'
+//const text = 'Never forget what you are, for surely the world will not'
+const motto = 'Family, Duty, Honor'
+let firstName = '  Grigor      \n'
+const name_1 = 'Tirion'
+const text = 'When you play the game, you win even if you don\'t want to win.'
 // BEGIN (write your solution here)
-console.log(one[2] + two[1] + three[3] +two[4] +two[2])
-console.log(`${one[2]}${two[1]}${three[3]}${two[4]}${two[2]}`)
+//console.log(one[2] + two[1] + three[3] +two[4] +two[2])
+//console.log(`${one[2]}${two[1]}${three[3]}${two[4]}${two[2]}`)
+//console.log(Math.pow(2, 3))
+//console.log(Math.abs(fruitsCount))
+//console.log(`First: ${text[0]}\nLast: ${text[55]}`)
+//console.log(Math.min (3, 10, 22, -3, 0))
+//console.log(Math.max (3, 10, 22, -3, 0))
+//console.log(Math.floor(Math.random() * 121,0))
+//console.log(typeof(motto))
+firstName = firstName.trim()
+//console.log(firstName)
+//console.log(name_1.toUpperCase().toLowerCase())
+console.log(text.slice(5,13).replace("you","someone").length)
+
+//console.log(text.length)
 // END
