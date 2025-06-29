311f52e30713f74a3f87e6d2c6bc0c96aab1a649
Makarova-Lida <lida-km@ya.ru>
2025-06-29 14:08:13 +0300

    изучение математических функций

--- proba_v2.js
+++ proba_v2.js
@@ -1,8 +1,17 @@
 const one = 'Naharis'
 const two = 'Mormont'
 const three = 'Sand'
-
+const fruitsCount = -2309
+const number = 923.2238
+const name = 'JavaScript'
+const text = 'Never forget what you are, for surely the world will not'
 // BEGIN (write your solution here)
-console.log(one[2] + two[1] + three[3] +two[4] +two[2])
-console.log(`${one[2]}${two[1]}${three[3]}${two[4]}${two[2]}`)
+//console.log(one[2] + two[1] + three[3] +two[4] +two[2])
+//console.log(`${one[2]}${two[1]}${three[3]}${two[4]}${two[2]}`)
+//console.log(Math.pow(2, 3))
+//console.log(Math.abs(fruitsCount))
+//console.log(Math.ceil(number))
+console.log(`First: ${text[0]}\nLast: ${text[55]}`)
+console.log(Math.min (3, 10, 22, -3, 0))
+console.log(Math.max (3, 10, 22, -3, 0))
 // END
